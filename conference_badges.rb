b51b038038a0e600ad33e4ec133f860cc829156a
John Guest <jcguest90@gmail.com>
2019-08-21 16:16:17 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,9 +5,7 @@ def badge_maker(name)
 end  
 
 def batch_badge_creator(names)
-  names.map do |name|
-  "Hello, my name is #{name}."
-   end 
+  names.map { |name| badge_maker(name)}
  end  
  
 # def assign_rooms(names)
